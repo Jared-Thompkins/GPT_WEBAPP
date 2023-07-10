3ca58b530fa6206c4332c73d65228abb1c030673
Jared Thompkins <jaredthompkins21@gmail.com>
2023-07-10 08:43:36 -0500

    add gpt output formatting

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_KEY')
 
+def format_paragraphs(text):
+    paragraphs = text.split('\n')
+    for i in range(len(paragraphs)):
+        parts = paragraphs[i].split('**')
+    for j in range(len(parts)):
+        if j % 2 == 1: 
+            parts[j] = f'<b>{parts[j]}</b>'
+    paragraphs[i] = ''.join(parts)
+
+    return '<p>' + '</p><p>'.join(paragraphs) + '</p>'
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
@@ -24,8 +35,9 @@ def index():
         )
         
         generated_response = response['choices'][0]['message']['content']
+        formatted_response = format_paragraphs(generated_response)
 
-        return redirect(url_for("index", result=generated_response))
+        return redirect(url_for("index", result=formatted_response))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
